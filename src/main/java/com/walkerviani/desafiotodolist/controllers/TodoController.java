2ba8006459692a4ea706ed8244c0b6ee39bf9465
Walker Viani <walkeryslan@gmail.com>
2026-01-06 19:50:01 -0300

    TodoController

---
+++ src/main/java/com/walkerviani/desafiotodolist/controllers/TodoController.java
@@ -0,0 +1,38 @@
+package com.walkerviani.desafiotodolist.controllers;
+
+import com.walkerviani.desafiotodolist.entities.Todo;
+import com.walkerviani.desafiotodolist.services.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    List<Todo> create(@RequestBody Todo todo) {
+        return todoService.create(todo);
+    }
+
+    @GetMapping
+    List<Todo> list(Todo todo) {
+        return todoService.list();
+    }
+
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo) {
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable("id") Long id) {
+        return todoService.delete(id);
+    }
+}
